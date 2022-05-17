afc16aa86d3935f9aafd81121dbf4b7a2e56d0ef
ivanovkirilg <ivanovkirilg@gmail.com>
2022-05-17 21:16:20 +0300

    Init
    LightHouse task ..*.... and gitignore

---
+++ LightHouse.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    while (1)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (i == j)
+                {
+                    putchar('*');
+                }
+                else
+                {
+                    putchar('.');
+                }
+            }
+
+            putchar('\r');
+            fflush(stdout);
+            sleep(1);
+        }
+    }
+
+    return 0;
+}
