4714ee1dd1094eeb37e300f3f29b3941f800343c
ivanovkirilg <ivanovkirilg@gmail.com>
2022-05-22 16:42:13 +0300

    Add naive and bitwise solutions to unique rows task

---
+++ 05_20/UniqueRows.c
@@ -0,0 +1,79 @@
+/*
+20.05.2022
+Задача 6*. Дом Напишете програма, която печата броя на уникалните редове в бинарна
+матрица, съставена само от единици и нули.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int count_unique_rows(int rows, int cols, const int matrix[rows][cols]);
+
+int main()
+{
+    int matrix[][6] = {
+        { 0, 1, 0, 1, 0, 1 }, // 1
+        { 0, 1, 0, 1, 0, 0 }, // 2
+        { 0, 1, 0, 1, 0, 1 }, // repeat
+        { 0, 0, 0, 0, 0, 0 }, // 3
+        { 1, 1, 1, 1, 1, 1 }, // 4
+        { 0, 0, 0, 0, 0, 0 }, // repeat
+        { 0, 1, 0, 1, 0, 1 }, // repeat
+    };
+
+    printf("Found %d unique rows.\n",
+        count_unique_rows(sizeof(matrix)/sizeof(matrix[0]), 6, matrix)
+    );
+
+    // == Performance test ==
+
+    // srand(2);
+    // int (*matrix)[32] = malloc(sizeof(int) * 50000 * 32);
+    // for (int i = 0; i < 50000; i++)
+    // {
+    //     for (int j = 0; j < 32; j++)
+    //     {
+    //         matrix[i][j] = rand() % 2;
+    //     }
+    // }
+    // printf("%d\n", count_unique_rows(50000, 32, matrix));
+}
+
+// Precondition: the matrix is binary
+int count_unique_rows(int rows, int cols, const int matrix[rows][cols])
+{
+    if (cols > 64)
+    {
+        printf("ERROR: Function not implemented for more than 64 columns.\n");
+        return -1;
+    }
+
+    int nr_of_unique_rows = 0;
+    uint64_t *cached_rows = calloc(rows, sizeof(uint64_t));
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            cached_rows[i] ^= (uint64_t) (matrix[i][j]) << j;
+        }
+
+        nr_of_unique_rows++;
+
+        for (int j = 0; j < i; j++)
+        {
+            if (cached_rows[i] == cached_rows[j])
+            {
+                nr_of_unique_rows--;
+                printf("Found duplicate row %d=%d: %.16lx.\n",
+                       i, j, cached_rows[i]);
+                break;
+            }
+        }
+    }
+
+    free(cached_rows);
+
+    return nr_of_unique_rows;
+}
---
+++ 05_20/UniqueRowsNaive.c
@@ -0,0 +1,76 @@
+/*
+20.05.2022
+Задача 6*. Дом Напишете програма, която печата броя на уникалните редове в бинарна
+матрица, съставена само от единици и нули.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int count_unique_rows_naive(int rows, int cols, const int matrix[rows][cols]);
+
+int main()
+{
+    int matrix[][6] = {
+        { 0, 1, 0, 1, 0, 1 }, // 1
+        { 0, 1, 0, 1, 0, 0 }, // 2
+        { 0, 1, 0, 1, 0, 1 }, // repeat
+        { 0, 0, 0, 0, 0, 0 }, // 3
+        { 1, 1, 1, 1, 1, 1 }, // 4
+        { 0, 0, 0, 0, 0, 0 }, // repeat
+        { 0, 1, 0, 1, 0, 1 }, // repeat
+    };
+
+    printf("Found %d unique rows.\n",
+        count_unique_rows_naive(sizeof(matrix)/sizeof(matrix[0]), 6, matrix)
+    );
+
+    // == Performance test ==
+
+    // srand(2);
+    // int (*matrix)[32] = malloc(sizeof(int) * 50000 * 32);
+    // for (int i = 0; i < 50000; i++)
+    // {
+    //     for (int j = 0; j < 32; j++)
+    //     {
+    //         matrix[i][j] = rand() % 2;
+    //     }
+    // }
+    // printf("%d\n", count_unique_rows_naive(50000, 32, matrix));
+}
+
+bool rows_equal(int size, const int left[size], const int right[size])
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (left[i] != right[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int count_unique_rows_naive(int rows, int cols, const int matrix[rows][cols])
+{
+    int nr_of_unique_rows = 0;
+
+    for (int i = 0; i < rows; i++)
+    {
+        nr_of_unique_rows++;
+
+        for (int j = 0; j < i; j++)
+        {
+            if (rows_equal(cols, matrix[i], matrix[j]))
+            {
+                nr_of_unique_rows--;
+                printf("Found duplicate row %d=%d.\n", i, j);
+                break;
+            }
+        }
+    }
+
+    return nr_of_unique_rows;
+}
